9055c2e68ebca7a607a7533982f79de7b53a2450
ark <72707293+VeryUsual@users.noreply.github.com>
2026-05-23 22:30:02 -0400

    main menu

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 230.0
+const JUMP_VELOCITY = -370.0
 
 func _physics_process(delta: float) -> void:
 	# Gravity
