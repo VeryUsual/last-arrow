52a3dd8e97f89dbe4dafb085310d5a81135b6a75
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-05-24 15:16:08 -0400

    arrows left & main menu button tweening

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,35 @@
 extends Control
 
+var play_btn_tween: Tween
+var quit_btn_tween: Tween
+
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_play_button_mouse_entered() -> void:
+	if play_btn_tween and play_btn_tween.is_running():
+		play_btn_tween.kill()
+	play_btn_tween = create_tween()
+	play_btn_tween.tween_property($VBoxContainer/PlayButton, "scale", Vector2(1.1, 1.1), 0.1).set_trans(Tween.TRANS_ELASTIC)
+
+func _on_play_button_mouse_exited() -> void:
+	if play_btn_tween and play_btn_tween.is_running():
+		play_btn_tween.kill()
+	play_btn_tween = create_tween()
+	play_btn_tween.tween_property($VBoxContainer/PlayButton, "scale", Vector2(1.0, 1.0), 0.1).set_trans(Tween.TRANS_ELASTIC)
+
+func _on_quit_button_mouse_entered() -> void:
+	if quit_btn_tween and quit_btn_tween.is_running():
+		quit_btn_tween.kill()
+	quit_btn_tween = create_tween()
+	quit_btn_tween.tween_property($VBoxContainer/QuitButton, "scale", Vector2(1.1, 1.1), 0.1).set_trans(Tween.TRANS_ELASTIC)
+
+func _on_quit_button_mouse_exited() -> void:
+	if quit_btn_tween and quit_btn_tween.is_running():
+		quit_btn_tween.kill()
+	quit_btn_tween = create_tween()
+	quit_btn_tween.tween_property($VBoxContainer/QuitButton, "scale", Vector2(1.0, 1.0), 0.1).set_trans(Tween.TRANS_ELASTIC)
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,15 @@ var Arrow: PackedScene = preload("res://scenes/arrow.tscn")
 const SPEED = 230.0
 const JUMP_VELOCITY = -370.0
 var mouse_position_at_shoot_start
+var can_release_bow = false
+var can_shoot = true
+var arrows = 4
 
 func _ready() -> void:
 	$Bow/Line2D.visible = false
+
+func _process(delta: float) -> void:
+	$HUD/ArrowsLeftLabel.text = str(arrows) + " arrows left"
 
 func _physics_process(delta: float) -> void:
 	# Gravity
@@ -27,31 +33,41 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-	# Shooting
-	if Input.is_action_just_pressed("shoot"):
-		$Bow/Line2D.visible = true
-		$Bow.shooting = true
-		mouse_position_at_shoot_start = get_global_mouse_position()
-		if mouse_position_at_shoot_start.x < position.x:
-			$Bow/Line2D.rotation = -20
-		else:
-			$Bow/Line2D.rotation = 20
-	
-	if Input.is_action_pressed("shoot"):
-		if mouse_position_at_shoot_start.x < position.x:
-			if $Bow/Line2D.rotation < -17:
-				$Bow/Line2D.rotation += 0.02
-		else:
-			if $Bow/Line2D.rotation > 17:
-				$Bow/Line2D.rotation -= 0.02
-	
-	if Input.is_action_just_released("shoot"):
-		$Bow/Line2D.visible = false
-		shoot()
-		$Bow.shooting = false
+	if arrows > 0:
+		# Shooting
+		if Input.is_action_just_pressed("shoot") and can_shoot:
+			$Bow/Line2D.visible = true
+			$Bow.shooting = true
+			mouse_position_at_shoot_start = get_global_mouse_position()
+			if mouse_position_at_shoot_start.x < position.x:
+				$Bow/Line2D.rotation = -20
+			else:
+				$Bow/Line2D.rotation = 20
+			can_release_bow = false
+			can_shoot = false
+			await get_tree().create_timer(0.6).timeout
+			can_release_bow = true
+			await get_tree().create_timer(0.2).timeout
+			can_shoot = true
+		
+		if Input.is_action_pressed("shoot"):
+			if mouse_position_at_shoot_start.x < position.x:
+				if $Bow/Line2D.rotation < -17:
+					$Bow/Line2D.rotation += 0.02
+			else:
+				if $Bow/Line2D.rotation > 17:
+					$Bow/Line2D.rotation -= 0.02
+		
+		if Input.is_action_just_released("shoot"):
+			$Bow/Line2D.visible = false
+			if can_release_bow:
+				shoot()
+			$Bow.shooting = false
 
 func shoot():
-	var a = Arrow.instantiate()
-	get_tree().current_scene.add_child(a)
-	a.global_position = $Bow.global_position
-	a.global_rotation = $Bow/Line2D.global_rotation
+	if arrows > 0:
+		arrows -= 1
+		var a = Arrow.instantiate()
+		get_tree().current_scene.add_child(a)
+		a.global_position = $Bow.global_position
+		a.global_rotation = $Bow/Line2D.global_rotation
