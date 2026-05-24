d0b9106080c6c29dcc93e0c412e282248aa94d1b
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-05-24 16:33:38 -0400

    coyote jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 var Arrow: PackedScene = preload("res://scenes/arrow.tscn")
 const SPEED = 230.0
 const JUMP_VELOCITY = -370.0
@@ -8,9 +7,13 @@ var mouse_position_at_shoot_start
 var can_release_bow = false
 var can_shoot = true
 var arrows = 4
+var coyote_frames = 6
+var coyote = false
+var last_floor = false
 
 func _ready() -> void:
 	$Bow/Line2D.visible = false
+	$CoyoteTimer.wait_time = coyote_frames / 60.0
 
 func _process(delta: float) -> void:
 	$HUD/ArrowsLeftLabel.text = str(arrows) + " arrows left"
@@ -21,8 +24,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Jumping
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
 		velocity.y = JUMP_VELOCITY
+		coyote = false
+		$CoyoteTimer.stop()
 
 	# Movement
 	var direction := Input.get_axis("move_left", "move_right")
@@ -32,6 +37,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	if !is_on_floor() and last_floor:
+		coyote = true
+		$CoyoteTimer.start()
+	last_floor = is_on_floor()
 	
 	if arrows > 0:
 		# Shooting
@@ -71,3 +80,6 @@ func shoot():
 		get_tree().current_scene.add_child(a)
 		a.global_position = $Bow.global_position
 		a.global_rotation = $Bow/Line2D.global_rotation
+
+func _on_coyote_timer_timeout() -> void:
+	coyote = false
